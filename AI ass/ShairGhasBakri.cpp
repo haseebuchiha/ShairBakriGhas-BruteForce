fc0a573ef9fbee6405f39965730bd11c7bf19d14
haseebuchiha <haseeb9009@gmail.com>
2020-09-08 07:07:52 +0500

    Initial Commit

---
+++ AI ass/ShairGhasBakri.cpp
@@ -0,0 +1,20 @@
+
+#include "ShairGhasBakri.h"
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
+int main()
+{
+	char c;
+	c = 0x0f;
+	
+	
+	if (c == 0x0f)
+	{
+		cout << "Yoou win";
+	}
+	cout << c;
+	return 0;
+}
